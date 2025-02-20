b1db3282bdd0fbde07c3291fae4b2683dbb4ba19
Caio Barbosa <caiofelipebarbosa92@gmail.com>
2025-02-20 14:41:28 -0300

    Starting the project

---
+++ src/main/java/com/barbosa/listaContatos/ListaContatosApplication.java
@@ -0,0 +1,13 @@
+package com.barbosa.listaContatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListaContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListaContatosApplication.class, args);
+	}
+
+}
